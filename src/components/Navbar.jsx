2fabeb1402f5bff9fdaa6cb13569e66d86ff6feb
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-02-17 09:27:22 +0545

    install

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return (
+        <div>
+            <nav className="">
+                <h2>This is navbar</h2>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
