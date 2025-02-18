1fcb7989caf8e224ac5ab4615f95b6c4f5433b0c
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-02-18 06:55:01 +0545

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import logo from '../assets/logo.png';
+
 function Navbar(){
     return (
         <div>
-            <nav className="">
-                <h2>This is navbar</h2>
+            <nav className="flex justify-between items-center px-15 py-2 bg-gray-100">
+                <img src={logo} alt="logo" className="h-20" />
+                <ul className="flex justify-center space-x-4">
+                    <li><a href="#" className="text-blue-500">Home</a></li>
+                    <li><a href="#" className="text-blue-500">About</a></li>
+                    <li><a href="#" className="text-blue-500">Services</a></li>
+                    <li><a href="#" className="text-blue-500">Contact</a></li>
+                </ul>
             </nav>
         </div>
     )
