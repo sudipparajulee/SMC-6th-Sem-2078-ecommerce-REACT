84faed2d24f4f2d078eb9465d3c68c5d7a1238a6
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-02-23 06:57:32 +0545

    products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function ProductCard() {
+function ProductCard({ product }) {
   return (
     <div>
         <a href='' className='bg-white p-4 rounded-lg shadow-lg block hover:shadow-xl hover:-translate-y-1 transition duration-300'>
-            <img src='https://picsum.photos/500' alt='product' className='h-40 w-full object-cover' />
-            <h2 className='text-lg font-semibold mt-4'>Product Name</h2>
-            <p className='text-sm mt-2'>Product Description</p>
-            <p className='text-sm font-semibold mt-2'>$100</p>
+            <img src={product.image} alt='product' className='h-40 w-full object-cover' />
+            <h2 className='text-lg font-semibold mt-4'>{product.productname}</h2>
+            <p className='text-sm mt-2'>{product.description}</p>
+            <p className='text-sm font-semibold mt-2'>${product.price}</p>
         </a>
     </div>
   )
--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -2,13 +2,50 @@ import React from 'react'
 import ProductCard from './ProductCard'
 
 function ProductContainer() {
+  const products = [
+    {
+      id: 1,
+      productname: "Product A",
+      description: "this is description",
+      price:100,
+      image: 'https://picsum.photos/500'
+    },
+    {
+      id: 2,
+      productname: "Product 2",
+      description: "this is description",
+      price:200,
+      image: 'https://picsum.photos/600'
+    },
+    {
+      id: 3,
+      productname: "Product 3",
+      description: "this is description",
+      price:300,
+      image: 'https://picsum.photos/700'
+    },
+    {
+      id: 4,
+      productname: "Product 4",
+      description: "this is description",
+      price:400,
+      image: 'https://picsum.photos/800'
+    },
+    {
+      id: 5,
+      productname: "Product 5",
+      description: "this is description",
+      price:500,
+      image: 'https://picsum.photos/900'
+    }
+  ]
   return (
     <div>
         <div className='grid grid-cols-4 gap-4 px-20 py-10'>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {products.map((product) => (
+                // <ProductCard key={product.id} productname={product.productname} price={product.price} description={product.description} />
+                <ProductCard key={product.id} product={product} />
+            ))}
         </div>
     </div>
   )
